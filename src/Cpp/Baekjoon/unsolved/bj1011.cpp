b706253a0867ee02979112f054587d8c72de4304
말랑한 햄쯰 <wnsgnswls1@gmail.com>
2022-02-08 03:59:58 +0000

    백준1011:Fly me to Alpha Centauri

---
+++ src/Cpp/Baekjoon/unsolved/bj1011.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void) {
+	int t; cin>>t;
+	
+	int s,e;
+	int d;
+	int j;
+	int cnt=0;
+	
+	for(int i=0;i<t;i++) {
+		cin>>s>>e;
+		d = e-s;
+		
+		if(d<4) {
+			cout<<d<<endl;
+			continue;
+		}
+		
+		// 최대 제곱 구하기
+		int squ=1;
+		while(d < pow(squ,2) || pow(squ+1,2) <= d) {
+			squ++;
+		}
+		cnt += 2*(squ-1)+1;
+		d-=pow(squ,2);
+		
+		// 거리에서 제곱을 빼고 남은 거리를 처리하기
+		int j = squ;
+		while(d!=0) {
+			if(0<=d-j) {
+				d -= j;
+				cnt++;
+			} else {
+				j--;
+			}
+		}
+		
+		cout<<cnt<<endl;
+	}
+}
