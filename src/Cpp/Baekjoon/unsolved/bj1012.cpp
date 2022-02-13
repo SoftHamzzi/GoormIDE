409a9771087ff295167e081e6a4361896ce3b61c
말랑한 햄쯰 <wnsgnswls1@gmail.com>
2022-02-13 19:09:16 +0000

    백준1012:유기농 배추

--- src/Cpp/Baekjoon/unsolved/bj1012.cpp
+++ src/Cpp/Baekjoon/unsolved/bj1012.cpp
@@ -1,6 +1,5 @@
 // 유기농 배추 (계획에 오차있어서 코드 개판됨)
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
@@ -9,44 +8,33 @@ typedef struct _coordinate {
 	int y;
 } coordinate;
 
-int compare(coordinate cabbage1, coordinate cabbage2) {
-	if(cabbage1.y == cabbage2.y) {
-		return cabbage1.x < cabbage2.x;
-	} else {
-		return cabbage1.y < cabbage2.y;
-	}
-}
-
-// 주위를 탐색했을때 아무것도 없다면 0을 반납, 자연수를 반납했다면 그 숫자를 반납
-int search(int (*farm)[50], int m, int n, int x, int y) {
-	// 위
-	if(1<=y && 0<farm[y-1][x]) {
-		return farm[y-1][x];
-	}
-	// 오른쪽
-	if(x<=m-2 && 0<farm[y][x+1]) {
-		return farm[y][x+1];
-	}
-	// 아래
-	if(y<=n-2 && 0<farm[y+1][x]) {
-		return farm[y+1][x];
-	}
-	// 왼쪽
-	if(1<=x && 0<farm[y][x-1]) {
-		return farm[y][x-1];
+void dfs(int (*farm)[50], int m, int n, int x, int y) {
+	// 경계를 벗어났거나, 해당 장소가 0이라면 돌아감
+	if(x<0 || m<=x || y<0 || n<=y || !farm[y][x]) {
+		return;
 	}
 	
-	// 주위에 아무것도 없음
-	return 0;
+	farm[y][x]=0;
+	// 위쪽
+	dfs(farm,m,n,x,y-1);
+	// 오른쪽
+	dfs(farm,m,n,x+1,y);
+	// 아래쪽
+	dfs(farm,m,n,x,y+1);
+	// 왼쪽
+	dfs(farm,m,n,x-1,y);
+		
+	return;
 }
 
-void resetFarm(int (*farm)[50], int m, int n) {
-	for(int i=0;i<n;i++) {
-		for(int j=0;j<m;j++) {
-			farm[i][j] = 0;
-		}
+/*
+for(int a=0;a<n;a++) {
+	for(int b=0;b<m;b++) {
+		cout<<farm[a][b];
 	}
+	cout<<"\n";
 }
+*/
 
 int main(void) {
 	int t; cin>>t; // 테스트 케이스 갯수
@@ -54,10 +42,8 @@ int main(void) {
 	int k; // 배추 갯수
 	int farm[50][50];
 	coordinate cabbage[2500];
-	
-	int ix,iy;
-	int x,y;
-	int rNum;
+
+	int inputX,inputY;
 	int cnt=0;
 	// 테스트 케이스만큼 반복
 	for(int i=0;i<t;i++) {
@@ -66,30 +52,21 @@ int main(void) {
 		
 		// 배추 갯수만큼 배추 좌표 입력
 		for(int j=0;j<k;j++) {
-			cin>>ix>>iy;
-			cabbage[j].x=ix;
-			cabbage[j].y=iy;
-			farm[iy][ix]=-1;
+			cin>>inputX>>inputY;
+			cabbage[j].x=inputX;
+			cabbage[j].y=inputY;
+			farm[inputY][inputX]=1;
 		}
 		
-		sort(&cabbage[0], &cabbage[k], compare);
-		
-		for(int l=0;l<k;l++) {
-			// 0을 받으면 주위에 아무것도 없다는 뜻이니 cnt를 넣는다.
-			// 자연수를 받으면 그 자연수를 넣는다.
-			x = cabbage[l].x;
-			y = cabbage[l].y;
-			
-			rNum = search(farm,m,n,x,y);
-			if(rNum == 0) {
-				farm[y][x] = ++cnt;
-			} else {
-				farm[y][x] = rNum;
+		// 배추 갯수만큼 DFS 돌리기
+		for(int j=0;j<k;j++) {
+			// 배추 위치를 살펴봤을때 1일 경우만 dfs를 돌림
+			if(farm[cabbage[j].y][cabbage[j].x]) {
+				dfs(farm, m, n, cabbage[j].x, cabbage[j].y);
+				cnt++;
 			}
 		}
 		cout<<cnt<<"\n";
-		
 		cnt=0;
-		resetFarm(farm,m,n);
 	}
 }
