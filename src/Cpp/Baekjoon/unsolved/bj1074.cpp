54e001cb2f3f9d8514bff2f1923dd377bd7f93fe
말랑한 햄쯰 <wnsgnswls1@gmail.com>
2022-02-16 00:05:37 +0000

    백준1074:Z

--- src/Cpp/Baekjoon/unsolved/bj1074.cpp
+++ src/Cpp/Baekjoon/unsolved/bj1074.cpp
@@ -1,3 +1,4 @@
+// 분할정복 + 재귀
 #include <iostream>
 #include <cmath>
 
@@ -7,11 +8,24 @@ int n,r,c;
 int cnt=0;
 
 void recursion(int y, int x, int len) {
+	if(r==y && c==x) {
+		cout<<cnt<<endl;
+		return;
+	}
 	
+	if(y <= r && r < y+len && x <= c && c < x+len) {
+		recursion(y,x,len/2);
+		recursion(y,x+(len/2),len/2);
+		recursion(y+(len/2),x,len/2);
+		recursion(y+(len/2),x+(len/2),len/2);
+	} else {
+		cnt += len*len;
+		return;
+	}
 }
 
 int main(void) {
 	cin>>n>>r>>c;
 	
-	recursion(0,0,1);
+	recursion(0,0,pow(2,n));
 }
